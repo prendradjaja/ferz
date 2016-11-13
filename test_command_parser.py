e582ef20c64ff04f6d2a5e7cb0a23c13b9a06ffa
Pandu Rendradjaja <prendradjaja@gmail.com>
2016-11-12 23:54:09 -0800

    Change naming scheme for Filters and Commands (#9)
    commands.RootCommand => commands.Root
    filters.TimeControlFilter => filters.TimeControl

--- test_command_parser.py
+++ test_command_parser.py
@@ -1,56 +1,54 @@
 from command_parser import parse
-from commands import (DaysCommand, FrequentCommand, HumanCommand,
-        MonthsCommand, MoveCommand, RatedCommand, RootCommand,
-        TimeControlCommand, UpCommand, YearsCommand)
+import commands
 import pytest
 
 
 @pytest.mark.parametrize('input,expected', [
-    ('', FrequentCommand(0)),
-    ('0', FrequentCommand(0)),
-    ('1', FrequentCommand(1)),
-    ('5', FrequentCommand(5)),
-    ('9', FrequentCommand(9)),
+    ('', commands.Frequent(0)),
+    ('0', commands.Frequent(0)),
+    ('1', commands.Frequent(1)),
+    ('5', commands.Frequent(5)),
+    ('9', commands.Frequent(9)),
     # but not multiple digits
 
-    ('h', HumanCommand()),
-    ('r', RatedCommand()),
-    ('/', RootCommand()),
+    ('h', commands.Human()),
+    ('r', commands.Rated()),
+    ('/', commands.Root()),
 
-    ('-', UpCommand(1)),
-    ('--', UpCommand(2)),
-    ('-----', UpCommand(5)),
+    ('-', commands.Up(1)),
+    ('--', commands.Up(2)),
+    ('-----', commands.Up(5)),
 
-    ('2t', TimeControlCommand(2)),
-    ('20t', TimeControlCommand(20)),
+    ('2t', commands.TimeControl(2)),
+    ('20t', commands.TimeControl(20)),
     # but not 't' itself
 
-    ('3d', DaysCommand(3)),
-    ('21d', DaysCommand(21)),
+    ('3d', commands.Days(3)),
+    ('21d', commands.Days(21)),
     # but not 'd' itself
-    ('7m', MonthsCommand(7)),
-    ('77777m', MonthsCommand(77777)),
-    ('2y', YearsCommand(2)),
-    ('100y', YearsCommand(100)),
+    ('7m', commands.Months(7)),
+    ('77777m', commands.Months(77777)),
+    ('2y', commands.Years(2)),
+    ('100y', commands.Years(100)),
 
     # actual algebraic moves
-    ('e4', MoveCommand('e4')),
-    ('g6', MoveCommand('g6')),
-    ('Nf3', MoveCommand('Nf3')),
-    ('O-O', MoveCommand('O-O')),
-    ('O-O-O', MoveCommand('O-O-O')),
-    ('O-O#', MoveCommand('O-O#')),
-    ('Bg8+', MoveCommand('Bg8+')),
+    ('e4', commands.Move('e4')),
+    ('g6', commands.Move('g6')),
+    ('Nf3', commands.Move('Nf3')),
+    ('O-O', commands.Move('O-O')),
+    ('O-O-O', commands.Move('O-O-O')),
+    ('O-O#', commands.Move('O-O#')),
+    ('Bg8+', commands.Move('Bg8+')),
     # and many more...
 
     # things that aren't actually algebraic moves but aren't valid commands
     # (currently we just parse all of these as algebraic moves)
-    ('t', MoveCommand('t')),
-    ('d', MoveCommand('d')),
-    ('m', MoveCommand('m')),
-    ('y', MoveCommand('y')),
-    ('13', MoveCommand('13')),
-    ('1000', MoveCommand('1000')),
+    ('t', commands.Move('t')),
+    ('d', commands.Move('d')),
+    ('m', commands.Move('m')),
+    ('y', commands.Move('y')),
+    ('13', commands.Move('13')),
+    ('1000', commands.Move('1000')),
 ])
 def test_parse(input, expected):
     assert parse(input) == expected
